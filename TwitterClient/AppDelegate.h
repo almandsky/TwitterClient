65001fdc91fe2c93ae577eb0de5ef0ee7c1dcdfd
Sky Chen <skychen@yahoo-inc.com>
2016-09-19 14:50:57 -0700

    add project files

---
+++ TwitterClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterClient
+//
+//  Created by Sky Chen on 9/19/16.
+//  Copyright © 2016 Sky Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterClient
+//
+//  Created by Sky Chen on 9/19/16.
+//  Copyright © 2016 Sky Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
